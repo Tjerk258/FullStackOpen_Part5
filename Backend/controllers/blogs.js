9edef7e99bcdd5ac6bc4976a86dd5a70e920aad2
Tjerk <tjtd1234@gmail.com>
2024-01-14 11:24:07 +0100

    part 4

---
+++ Backend/controllers/blogs.js
@@ -0,0 +1,61 @@
+const blogRouter = require('express').Router()
+require('express-async-errors')
+const Blog = require('../models/blogs')
+
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
+})
+
+blogRouter.post('/', async (request, response) => {
+  if(!request.user) {
+    return response.status(401).send('For this you need to be logged in')
+  }
+  const user = request.user
+  const body = request.body
+  const blog = new Blog({
+    title: body.title,
+    url: body.url,
+    author: body.author,
+    likes: body.likes | 0,
+    user: user._id
+  })
+  const result = await blog.save()
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
+  response.status(201).json(result)
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  if(!request.user) {
+    return response.status(401).send('For this you need to be logged in')
+  }
+  if(!request.user.blogs.find(blog => String(blog) === request.params.id)) {
+    return response.status(401).send('You dont own this blog')
+  }
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  if(!request.user) {
+    return response.status(401).send('For this you need to be logged in')
+  }else if(!request.user.blogs.find(blog => String(blog) === request.params.id)) {
+    return response.status(404).send('non existent ID')
+  }
+  const body = request.body
+  const blog = {
+    title: body.title,
+    url: body.url,
+    author: body.author,
+    likes: body.likes
+  }
+  const updatedNote = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true, runValidators: true, context: 'query' })
+  if(updatedNote) {
+    response.json(updatedNote)
+  } else {
+    response.status(404).send('non exitent ID')
+  }
+})
+
+module.exports = blogRouter
